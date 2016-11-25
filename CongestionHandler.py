43426479f1bee5cfbb9293e4ff5ee188dec01ad6
SudhakarReddyPeddinti <p.reddy.sudhakar@gmail.com>
2016-11-25 00:55:37 -0600

    Hop Count implementation

--- CongestionHandler.py
+++ CongestionHandler.py
@@ -3,11 +3,14 @@ INF = 9999999
 
 distance = [[0 for row in range(6)] for col in range(6)]
 nextVertex = [[0 for row in range(6)] for col in range(6)]
+hopCount = [[0 for row in range(6)] for col in range(6)]
 
 def printShortestDistance(distGraph):
-    print("Floyd Warshall Graph Distances")
+    print("Floyd Warshall Graph Distances:")
     print('\n'.join([''.join(['{:5}'.format(item) for item in row])
                      for row in distGraph]))
+    print("\n")
+    print("\n")
 
 
 def get_path(origin, dest):
@@ -15,27 +18,37 @@ def get_path(origin, dest):
   Reconstruct shortest path from origin to destination using the predecessor matrix passed as input
   This method is not recursive to avoid reach  of call stack limit when input is a large matrix
   """
+  u = origin - 1
+  v = dest - 1
+
   path_list = [origin]
   while 1:
     predecessor = nextVertex[origin - 1][dest - 1] + 1
     if predecessor == origin:
       return map(str, path_list)
     path_list.append(int(predecessor))
+    hopCount[u][v] = hopCount[u][v]+1
     origin = predecessor
 
 
 def printShortestPath():
-    shortestPath = [[0 for i in range(6)] for j in range(6)]
-    print(" ")
     print("Floyd Warshall Graph Paths")
     for u, i in enumerate(range(0, 6), 1):
         for v, j in enumerate(range(0, 6), 1):
             print(','.join(get_path(u, v)), end="      ")
         print("\n")
+    print("\n")
+
+
+def printHopCount():
+    print("Hop count:")
+    for u in range(6):
+        for v in range(6):
+            print(hopCount[u][v], end="  ")
+        print(" ")
 
 
 def floydWarshall(EdgeGraph):
-
     # Initialize the edge weights to distances.
     for u in range(6):
         for v in range(6):
@@ -50,6 +63,7 @@ def floydWarshall(EdgeGraph):
                     nextVertex[i][j] = nextVertex[i][k]
     printShortestDistance(distance)
     printShortestPath()
+    printHopCount()
 
 if __name__ == '__main__':
     # Edge weight input filling Column -> Rows values
